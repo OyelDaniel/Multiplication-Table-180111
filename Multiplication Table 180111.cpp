55217f446adbbdd1699456983c29fe2232ec0311
OyelDaniel <102643280+OyelDaniel@users.noreply.github.com>
2022-10-26 00:02:39 +0800

    Add files via upload

---
+++ Multiplication Table 180111.cpp
@@ -0,0 +1,52 @@
+#include"stdio.h"
+int main()
+{
+	int nomborSifir = 3;
+	printf("sifir 3");
+	printf("\n_______");
+	printf("\n\n1 * %d = %d", nomborSifir, 1 * nomborSifir);
+	printf("\n\n2 * %d = %d", nomborSifir, 2 * nomborSifir);
+	printf("\n\n3 * %d = %d", nomborSifir, 3 * nomborSifir);
+	printf("\n\n4 * %d = %d", nomborSifir, 4 * nomborSifir);
+	printf("\n\n5 * %d = %d", nomborSifir, 5 * nomborSifir);
+	printf("\n\n6 * %d = %d", nomborSifir, 6 * nomborSifir);
+	printf("\n\n7 * %d = %d", nomborSifir, 7 * nomborSifir);
+	printf("\n\n8 * %d = %d", nomborSifir, 8 * nomborSifir);
+	printf("\n\n9 * %d = %d", nomborSifir, 9 * nomborSifir);
+	printf("\n\n10 * %d = %d", nomborSifir, 10 * nomborSifir);
+	printf("\n\n11 * %d = %d", nomborSifir, 11 * nomborSifir);
+	printf("\n\n12 * %d = %d", nomborSifir, 12 * nomborSifir);
+	
+	int nomboSifir = 4;
+	printf("\n\nsifir 4");
+	printf("\n_______");
+	printf("\n\n1 * %d = %d", nomboSifir, 1 * nomboSifir);
+	printf("\n\n2 * %d = %d", nomboSifir, 2 * nomboSifir);
+	printf("\n\n3 * %d = %d", nomboSifir, 3 * nomboSifir);
+	printf("\n\n4 * %d = %d", nomboSifir, 4 * nomboSifir);
+	printf("\n\n5 * %d = %d", nomboSifir, 5 * nomboSifir);
+	printf("\n\n6 * %d = %d", nomboSifir, 6 * nomboSifir);
+	printf("\n\n7 * %d = %d", nomboSifir, 7 * nomboSifir);
+	printf("\n\n8 * %d = %d", nomboSifir, 8 * nomboSifir);
+	printf("\n\n9 * %d = %d", nomboSifir, 9 * nomboSifir);
+	printf("\n\n10 * %d = %d", nomboSifir, 10 * nomboSifir);
+	printf("\n\n11 * %d = %d", nomboSifir, 11 * nomboSifir);
+	printf("\n\n12 * %d = %d", nomboSifir, 12 * nomboSifir);
+	
+	int nombSifir = 5;
+	printf("\n\nsifir 5");
+	printf("\n_______");
+	printf("\n\n1 * %d = %d", nombSifir, 1 * nombSifir);
+	printf("\n\n2 * %d = %d", nombSifir, 2 * nombSifir);
+	printf("\n\n3 * %d = %d", nombSifir, 3 * nombSifir);
+	printf("\n\n4 * %d = %d", nombSifir, 4 * nombSifir);
+	printf("\n\n5 * %d = %d", nombSifir, 5 * nombSifir);
+	printf("\n\n6 * %d = %d", nombSifir, 6 * nombSifir);
+	printf("\n\n7 * %d = %d", nombSifir, 7 * nombSifir);
+	printf("\n\n8 * %d = %d", nombSifir, 8 * nombSifir);
+	printf("\n\n9 * %d = %d", nombSifir, 9 * nombSifir);
+	printf("\n\n10 * %d = %d", nombSifir, 10 * nombSifir);
+	printf("\n\n11 * %d = %d", nombSifir, 11 * nombSifir);
+	printf("\n\n12 * %d = %d", nombSifir, 12 * nombSifir);
+	return 0;	
+}
